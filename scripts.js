aa707edb18eff5ad40462d6785e79a36fe548901
cTM73 <104709562+cTM73@users.noreply.github.com>
2023-08-06 19:28:26 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let search = document.querySelector(".prank")
+let alert = 0
+
+search.addEventListener("click", () => {
+    if (alert == 0) {
+        alert("BRO FELL FOR IT LMAOOOOOO")
+    }
+    alert++
+    search.innerHTML = "LMAOO YOU THOUGHT"   
+})
